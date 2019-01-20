d57720d2335e8e28204164e00a62ff3abbff3ac6
Anthony Zheng <ajz003@ucla.edu>
2019-01-20 15:38:26 -0800

    fix formatting issue and NaN

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const client = require('twilio')(process.env.accountSid, process.env.authToken);
 
 router.get("/api/scrape", function (req, res) {
-    let numDocs;
+    let numDocs = 0;
     Post.find({}).then((docs) => {
         numDocs = docs.length
         axios.get("https://www.reddit.com/r/cscareerquestions/search.json?q=bootcamp&limit=10&sort=new&restrict_sr=1").then(function (response) {
@@ -24,12 +24,13 @@ router.get("/api/scrape", function (req, res) {
                             Post.find({}).then((documents) => {
                                 if (documents.length !== numDocs) {
                                     let numNew = parseInt(documents.length - numDocs)
-                                    let textMessage = "";
+                                    console.log(numNew)
+                                    let textMessage = "Good afternoon!";
                                     for (let i = documents.length - numNew + 1, j = 1; i < documents.length + 1 && j < 4; i++, j++) {
-                                        textMessage += "\n\n " + parseInt(j) + ". Title: " + documents[i].title + "\n Link: " + documents[i].link
+                                        textMessage += "\n\n" + parseInt(j) + ". Title: " + documents[i].title + "\nLink: " + documents[i].link
                                     }
                                     if (numNew > 3) {
-                                        textMessage += "\n\n ...and " + numNew - 3 + " more."
+                                        textMessage += "\n\n" + "...and " + (numNew-3) + " more."
                                     }
                                     client.messages.create({
                                         body: textMessage,
